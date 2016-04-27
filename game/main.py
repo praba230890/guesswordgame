4dc9204cfa0d7d98c3bd62755cb7fcca5d6cce85
Prabakaran Santhanam <praba230890@gmail.com>
2016-04-27 23:41:34 +0530

    remove the multiplayer option

--- game/main.py
+++ game/main.py
@@ -4,30 +4,14 @@ from getpass import getpass as get_keyword
 
 class GuessWord(object):
     
-    def __init__(self, player_one, player_two):
+    def __init__(self):
         self.word_list = ["jack", "back", "cock", "luck", "bang", "tool", "dogs", "bags", "life", "kick"]
-        self.player_one = Player(player_one)
-        self.player_two = Player(player_two)
-        self.players = [self.player_one, self.player_two]
-        self.player_names = [player.name for player in self.players]
-        self.computer = "Computer"
-        if self.computer in self.player_names:
-            self.mode = 1
-        else:
-            self.mode = 2
-        self.player_cursor = 0
-
-    def start_game(self):
-        if self.mode == 1:
-            self.current_player = self.player_one
-            self.other_player = self.player_two
-            self.word = ""
-        else:
-            self.current_player = self.players[self.player_cursor%len(self.players)]
-            self.other_player = self.players[(self.player_cursor+1)%len(self.players)]
-            self.word = get_keyword("Player %s, please enter the word for player %s to guess: " % (self.other_player.name, self.current_player.name) )
         self.key = True
         self.last_word = []
+        self.word = self.grab_word()
+        
+
+    def start_game(self):
         while self.key == True:
             self.star = 0
             self.exc = 0
@@ -41,29 +25,17 @@ class GuessWord(object):
 
             #grabbing the guess word and checking the word length
             while self.tmp_word_length != len(self.word):
-                self.tmp_word = raw_input("Enter your guess that must be containing "+ str(len(self.word)) +" letters: ")
+                self.tmp_word = raw_input(
+                    "Enter your guess that must be containing "+ str(len(self.word)) +" letters: "
+                    )
                 self.tmp_word_length = len(self.tmp_word)
 
             #Exact word match if block
             if self.word == self.tmp_word:
-                self.player_one.games_played += 1
-                self.player_two.games_played += 1
-                self.current_player.wins += 1
-                if self.mode == 2:
-                    self.player_cursor += 1
-                    self.current_player = self.players[(self.player_cursor)%len(self.players)]
-                    self.other_player = self.players[(self.player_cursor+1)%len(self.players)]
-                for player in self.players:
-                    print "For player: ", player.name
-                    print "Games Played: ", player.games_played
-                    print "Games Won: ", player.wins
                 self.new_key = int(raw_input("Congrats! you've got the right word. To continue playing the game please enter 1 and to quit enter 2: \n 1. play \n 2. quit \n"))
                 if self.new_key == 1:
                     self.last_word.append(self.word)
-                    if self.mode == 2:
-                        self.word = get_keyword("Player %s, please enter the word for player %s to guess: " % (self.other_player.name, self.current_player.name) )
-                    else:
-                        self.word = ""
+                    self.word = self.grab_word()
                     continue
                 else:
                     self.star = 0
@@ -107,9 +79,4 @@ class GuessWord(object):
             if new_tmp_word_count.has_key(char):
                 self.exc += min(new_tmp_word_count[char], new_word_count[char])
 
-class Player(object):
-    def __init__(self, name=None):
-        self.name = name
-        self.games_played = 0
-        self.wins = 0
-        self.lost = 0
+
