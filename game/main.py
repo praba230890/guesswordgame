65478e24c1fc834e43ab9dd3d00c8429fbe96f22
Prabakaran Santhanam <praba230890@gmail.com>
2016-05-11 21:10:19 +0530

    refactor: variable rename

--- game/main.py
+++ game/main.py
@@ -9,29 +9,24 @@ class GuessWord(object):
         self.key = True
         self.last_word = []
         self.word = self.grab_word()
-        
 
     def start_game(self):
         while self.key == True:
             self.star = 0
             self.exc = 0
-            self.tmp_word_length = 0
-            self.diff = []
-            self.diff_index = []
+            self.guess_word_length = 0
 
-            #checking and grabbing the game word
-            if self.word == "":
-                self.word = self.grab_word()
-
-            #grabbing the guess word and checking the word length
-            while self.tmp_word_length != len(self.word):
-                self.tmp_word = raw_input(
+            #grabbing the guess word from user and checking the word length
+            while self.guess_word_length != len(self.word):
+                if self.guess_word_length != 0:
+                    print "Please enter a %d letter word" % len(self.word)
+                self.guess_word = raw_input(
                     "Enter your guess that must be containing "+ str(len(self.word)) +" letters: "
                     )
-                self.tmp_word_length = len(self.tmp_word)
+                self.guess_word_length = len(self.guess_word)
 
             #Exact word match if block
-            if self.word == self.tmp_word:
+            if self.word == self.guess_word:
                 self.new_key = int(raw_input("Congrats! you've got the right word. To continue playing the game please enter 1 and to quit enter 2: \n 1. play \n 2. quit \n"))
                 if self.new_key == 1:
                     self.last_word.append(self.word)
@@ -54,29 +49,26 @@ class GuessWord(object):
 
     def grab_word(self):
         while True:
-            word_no = random.randint(0,len(self.word_list)-1)
+            word_no = random.randint(0, len(self.word_list)-1)
             word = self.word_list[word_no]
-            if word in self.last_word:
-                pass
-            else:
-                break
-        return word
+            if word not in self.last_word:
+                return word
 
     def calculate_star(self):
         self.new_word = ""
-        self.new_tmp_word = ""
+        self.new_guess_word = ""
         for i, char in enumerate(self.word):
-            if self.tmp_word[i] == char:
+            if self.guess_word[i] == char:
                 self.star += 1
                 continue
             self.new_word += char
-            self.new_tmp_word += self.tmp_word[i]
+            self.new_guess_word += self.guess_word[i]
 
     def calculate_exclamation(self):
         new_word_count = dict(collections.Counter(self.new_word))
-        new_tmp_word_count = dict(collections.Counter(self.new_tmp_word))
+        new_guess_word_count = dict(collections.Counter(self.new_guess_word))
         for char in new_word_count:
-            if new_tmp_word_count.has_key(char):
-                self.exc += min(new_tmp_word_count[char], new_word_count[char])
+            if new_guess_word_count.has_key(char):
+                self.exc += min(new_guess_word_count[char], new_word_count[char])
 
 
